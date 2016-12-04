4a38216e028d56feadba02383f83dfcb7fe20399
JanikRitz <janikritz@gmail.com>
2016-12-04 19:14:19 +0100

    Added Shortened Create Game Room Feature File Added Step Definitions

---
+++ src/test/java/com/factracing/cucumber/MyStepdefs.java
@@ -0,0 +1,36 @@
+package com.factracing.cucumber;
+
+import cucumber.annotation.en.Given;
+import cucumber.runtime.PendingException;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by janik on 04/12/2016.
+ */
+
+public class MyStepdefs {
+
+
+    @Given("^Session with Name$")
+    public void sessionWithName() throws Throwable {
+        //Move
+        String chromeDriverLocation = "SeleniumChrome/chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", chromeDriverLocation);
+        //
+        WebDriver chrome = new ChromeDriver();
+        chrome.get("http://the-internet.herokuapp.com/");
+        assertTrue(chrome.getTitle().equals("The Internet"));
+        chrome.quit();
+        //throw new PendingException();
+    }
+
+    @Given("^Session$")
+    public void session() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
---
+++ src/test/resources/de/com/factracing/cucumber/CreateGameRoom.feature
@@ -0,0 +1,11 @@
+#Author: janikritz@gmail.com
+
+
+
+Feature: Create Game Room
+  as a User
+  I want to create a Game Room with my Configurations
+
+  Scenario: Valid Inputs
+    Given Session with Name
+
