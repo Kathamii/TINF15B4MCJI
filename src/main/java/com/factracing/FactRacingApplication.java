f7b11935740f3dc26643d7c93c95ffe38a781930
Kahmul <kahmul78@web.de>
2017-05-09 21:05:14 +0200

    Vaadin Spring Boot Setup
    Change-Id: Ic20322c31db65d241dadf197e8b7798589dc9421

---
+++ src/main/java/com/factracing/FactRacingApplication.java
@@ -0,0 +1,18 @@
+package com.factracing;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication
+public class FactRacingApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(FactRacingApplication.class, args);
+	}
+}
--- src/main/java/com/factracing/FactRacingUI.java
+++ src/main/java/com/factracing/FactRacingUI.java
@@ -1,58 +1,53 @@
 package com.factracing;
 
-import javax.servlet.annotation.WebServlet;
 
 import org.springframework.context.annotation.Configuration;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
 import com.vaadin.spring.annotation.EnableVaadin;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.spring.server.SpringVaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
- * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * This UI is the application entry point. A UI may either represent a browser window (or tab) or some part of a html page where a
+ * Vaadin application is embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
- * overridden to add component to the user interface and initialize non-component functionality.
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be overridden to add component to the user
+ * interface and initialize non-component functionality.
  */
 @Theme("factracingtheme")
 @SpringUI
-public class FactRacingUI extends UI {
+public class FactRacingUI extends UI
+{
 
-    @Configuration
-    @EnableVaadin
-    public static class MyConfiguration {
-    }
+	@Configuration
+	@EnableVaadin
+	public static class MyConfiguration
+	{
+	}
 
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
-    	
-    	SpringContextHelper helper = new SpringContextHelper(VaadinServlet.getCurrent().getServletContext());
-        final VerticalLayout layout = new VerticalLayout();
 
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+	@Override
+	protected void init(VaadinRequest vaadinRequest)
+	{
+		final VerticalLayout layout = new VerticalLayout();
 
-        Button button = new Button("Click Me");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue()
-                    + ", it works!"));
-        });
+		final TextField name = new TextField();
+		name.setCaption("Type your name here:");
 
-        layout.addComponents(name, button);
+		Button button = new Button("Click Me");
+		button.addClickListener(e -> {
+			layout.addComponent(new Label("Thanks " + name.getValue() + ", it works!"));
+		});
 
-        setContent(layout);
-    }
+		layout.addComponents(name, button);
 
-    @WebServlet(urlPatterns = "/*", name = "FactRacingUIServlet", asyncSupported = true)
-    public static class FactRacingUIServlet extends SpringVaadinServlet {
-    }
+		setContent(layout);
+	}
 }
