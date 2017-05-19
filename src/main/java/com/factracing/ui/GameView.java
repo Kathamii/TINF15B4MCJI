857a214401c21d8b6d896c75e2335d031d29b574
Kahmul <kahmul78@web.de>
2017-05-19 13:23:17 +0200

    Added first layout structure to GameView
    Change-Id: Id3b8b030af2ff49b435693f1008d4c61d9220e40

--- src/main/java/com/factracing/ui/GameView.java
+++ src/main/java/com/factracing/ui/GameView.java
@@ -3,19 +3,82 @@ package com.factracing.ui;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
 
 
-@SpringView(name = GameView.VIEW_NAME) 
-public class GameView implements View
+@SpringView(name = GameView.VIEW_NAME)
+public class GameView extends VerticalLayout implements View
 {
-	
+
 	public static final String VIEW_NAME = "gameView";
+
+
+	public GameView()
+	{
+		setSpacing(true);
+		setMargin(true);
+		setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+	}
+
+
+	/**
+	 * Initializes layout on each enter.
+	 */
+	private void initializeLayout()
+	{
+		removeAllComponents();
+		Label factRacingLabel = new Label("<h1>Fact Racing<h1>", ContentMode.HTML);
+		Label roomLabel = new Label("<h2>Game Room<h2>", ContentMode.HTML);
+
+		HorizontalLayout gameFieldLayout = createNewGameFieldLayout();
+
+		addComponents(factRacingLabel, roomLabel, gameFieldLayout);
+	}
+
+
+	private HorizontalLayout createNewGameFieldLayout()
+	{
+		HorizontalLayout gameFieldLayout = new HorizontalLayout();
+		
+		VerticalLayout questionLayout = createQuestionLayout();
+		VerticalLayout gameLayout = createGameLayout();
+		VerticalLayout gameLogLayout = createGameLogLayout();
+		
+		gameFieldLayout.addComponents(questionLayout, gameLayout, gameLogLayout);
+
+		return gameFieldLayout;
+	}
+
+
+	private VerticalLayout createQuestionLayout()
+	{
+		VerticalLayout questionLayout = new VerticalLayout();
+		return questionLayout;
+	}
+
+
+	private VerticalLayout createGameLayout()
+	{
+		VerticalLayout gameLayout = new VerticalLayout();
+		return gameLayout;
+	}
+	
+	private VerticalLayout createGameLogLayout()
+	{
+		VerticalLayout gameLogLayout = new VerticalLayout();
+		return gameLogLayout;
+	}
+
 
 	@Override
 	public void enter(ViewChangeEvent event)
 	{
-
+		initializeLayout();
 	}
 
 }
