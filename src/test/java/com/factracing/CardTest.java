855936378259e51841ac614af17164eb88766864
JanikRitz <janikritz@gmail.com>
2017-05-29 16:09:18 +0200

    Added Object for Testing

--- src/test/java/com/factracing/CardTest.java
+++ src/test/java/com/factracing/CardTest.java
@@ -1,6 +1,8 @@
 package com.factracing;
 
 
+import com.factracing.beans.Difficulty;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.factracing.beans.Card;
@@ -10,10 +12,15 @@ import static org.junit.Assert.assertEquals;
 
 public class CardTest
 {
-
+    private static int DIFFICULTY = 2;
+    private static String QUESTION = "Where", CORRECT_ANSWER = "There";
+    private static String[] ANSWERS = {"Somewhere", "Nowhere", "Everywhere"};
+    private Card emptyCard = new Card("", new String[0], "", 0);
+    private Card simpleCard = new Card(QUESTION, ANSWERS, CORRECT_ANSWER, DIFFICULTY);
 
 	@Test
     public void difficultyTest(){
-        assertEquals(0, new Card("", new String[0], "", 0).getDifficulty());
+        assertEquals(0, emptyCard.getDifficulty());
+        assertEquals(DIFFICULTY, simpleCard.getDifficulty());
     }
 }
