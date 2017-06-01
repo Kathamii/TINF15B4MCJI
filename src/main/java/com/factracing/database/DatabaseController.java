f2510e93b4911d7462be84bc5005c18e83223a2d
Kahmul <kahmul78@web.de>
2017-06-01 21:13:09 +0200

    Formatting and SQL statement improved
    Change-Id: Icff90c5e95c3c0ae521937058341f660404e1329

--- src/main/java/com/factracing/database/DatabaseController.java
+++ src/main/java/com/factracing/database/DatabaseController.java
@@ -1,5 +1,6 @@
 package com.factracing.database;
 
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -19,8 +20,10 @@ import com.factracing.beans.GameRoom;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
+
 @Controller
-public class DatabaseController {
+public class DatabaseController
+{
 
 	@Value("${spring.datasource.url}")
 	private String dbUrl;
@@ -28,64 +31,102 @@ public class DatabaseController {
 	@Autowired
 	private static DataSource dataSource;
 
+
 	@Bean
-	public DataSource dataSource() throws SQLException {
-		if (dbUrl == null || dbUrl.isEmpty()) {
+	public DataSource dataSource() throws SQLException
+	{
+		if (dbUrl == null || dbUrl.isEmpty())
+		{
 			return new HikariDataSource();
-		} else {
+		}
+		else
+		{
 			HikariConfig config = new HikariConfig();
 			config.setJdbcUrl(dbUrl);
 			return new HikariDataSource(config);
 		}
 	}
 
+
 	@RequestMapping("/db")
-	String db(Map<String, Object> model) {
-		try (Connection connection = dataSource.getConnection()) {
-			try (Statement stmt = connection.createStatement()) {
+	String db(Map<String, Object> model)
+	{
+		try (Connection connection = dataSource.getConnection())
+		{
+			try (Statement stmt = connection.createStatement())
+			{
 				stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick TIMESTAMP)");
 				stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-				try (ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks")) {
+				try (ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks"))
+				{
 					ArrayList<String> output = new ArrayList<String>();
-					while (rs.next()) {
+					while (rs.next())
+					{
 						output.add("Read from DB: " + rs.getTimestamp("tick"));
 					}
 					model.put("records", output);
 					return "db";
-				} catch (Exception e) {
+				}
+				catch (Exception e)
+				{
 					return null;
 				}
-			} catch (Exception e) {
+			}
+			catch (Exception e)
+			{
 				return null;
 			}
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			return null;
 		}
 	}
 
-	public static boolean saveGameRoomToDB(GameRoom room) {
-		try (Connection connection = dataSource.getConnection()) {
-			try (Statement stmt = connection.createStatement()) {
+
+	public static boolean saveGameRoomToDB(GameRoom room)
+	{
+		try (Connection connection = dataSource.getConnection())
+		{
+			try (Statement stmt = connection.createStatement())
+			{
 				String roomID = room.getRoomID();
-				stmt.executeUpdate("INSERT INTO gameroom (gameroomId) VALUES (" + roomID + ");");
-			} catch (Exception e) {
+				int minPlayers = room.getMinPlayers();
+				int maxPlayers = room.getMaxPlayers();
+				int playerCount = room.getPlayerCount();
+				String creatorID = room.getCreator().getUserID();
+				stmt.executeUpdate("INSERT INTO gameroom (gameroomid, minplayers, maxplayers, playercount, creatorid) VALUES (" + roomID
+						+ ", " + minPlayers + ", " + maxPlayers + ", " + playerCount + ", " + creatorID + ");");
+			}
+			catch (Exception e)
+			{
 				return false;
 			}
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			return false;
 		}
 		return true;
 	}
 
-	public static boolean deleteGameRoomFromDB(GameRoom room) {
-		try (Connection connection = dataSource.getConnection()) {
-			try (Statement stmt = connection.createStatement()) {
+
+	public static boolean deleteGameRoomFromDB(GameRoom room)
+	{
+		try (Connection connection = dataSource.getConnection())
+		{
+			try (Statement stmt = connection.createStatement())
+			{
 				String roomID = room.getRoomID();
-				stmt.executeUpdate("DELETE FROM gameroom WHERE gameroomId = " + roomID + ";");
-			} catch (Exception e) {
+				stmt.executeUpdate("DELETE FROM gameroom WHERE gameroomid = " + roomID + ";");
+			}
+			catch (Exception e)
+			{
 				return false;
 			}
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			return false;
 		}
 		return true;
